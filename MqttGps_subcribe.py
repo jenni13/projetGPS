b7ada23358e58a188cba4b5e31b03f96d1e99bbd
camsi10 <camsi10@siame10.siame.univ-tlse3.fr>
2018-10-16 17:43:35 +0200

    subcribe modif

--- MqttGps_subcribe.py
+++ MqttGps_subcribe.py
@@ -3,7 +3,8 @@
 import paho.mqtt.client as mqtt
 import webbrowser
 
-MQTT_SERVER = "localhost"
+#MQTT_SERVER = "localhost"
+MQTT_SERVER = "192.168.0.88"
 MQTT_PATH = "LongLat"
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -16,7 +17,14 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    #print(msg.topic+" "+str(msg.payload)+" "+str(msg.payload))
+    msg.payload = msg.payload.decode("utf-8")
+    print(str(msg.payload))
+    url = "http://maps.google.com/maps?q="+str(msg.payload)
+    chrome_path = '/usr/bin/google-chrome %s'
+    webbrowser.get(chrome_path).open(url)
+
+
     # more callbacks, etc
 
 client = mqtt.Client()
@@ -29,11 +37,12 @@ client.connect(MQTT_SERVER, 1883, 60)
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-client.loop_forever()
-#client.loop(15)
-long= str(msg.payload)
-latt = str(msg.payload)
-url = "http://maps.google.com/maps?q="+str(long)+","+str(latt)
-chrome_path = '/usr/bin/google-chrome %s'
-webbrowser.get(chrome_path).open(url)
+#client.loop_forever()
 
+#long= msg.payload
+#latt = msg.payload
+   # url = "http://maps.google.com/maps?q="+str(long)+","+str(lat)
+   # chrome_path = '/usr/bin/google-chrome %s'
+   # webbrowser.get(chrome_path).open(url)
+
+client.loop_forever()
