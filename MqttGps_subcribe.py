0a1ac5430102e684955c59adc233a546bab979d2
Manfredo Jennifer <jennifer.manfredo@gmail.com>
2018-10-20 14:29:50 +0200

    ctrl c

--- MqttGps_subcribe.py
+++ MqttGps_subcribe.py
@@ -2,9 +2,11 @@
 
 import paho.mqtt.client as mqtt
 import webbrowser
-
+import sys
 #MQTT_SERVER = "localhost"
-MQTT_SERVER = "192.168.0.88"
+#fac:
+#MQTT_SERVER = "192.168.0.88"
+MQTT_SERVER = "192.168.1.68"
 MQTT_PATH = "LongLat"
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -18,6 +20,7 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     #print(msg.topic+" "+str(msg.payload)+" "+str(msg.payload))
+
     msg.payload = msg.payload.decode("utf-8")
     print(str(msg.payload))
     url = "http://maps.google.com/maps?q="+str(msg.payload)
@@ -26,23 +29,21 @@ def on_message(client, userdata, msg):
 
 
     # more callbacks, etc
+try:
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+    client.connect(MQTT_SERVER, 1883, 60)
+    client.loop_forever()
 
-client.connect(MQTT_SERVER, 1883, 60)
+except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print ("\nKilling Client...")
+    sys.exit(0)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
+
 #client.loop_forever()
-
-#long= msg.payload
-#latt = msg.payload
-   # url = "http://maps.google.com/maps?q="+str(long)+","+str(lat)
-   # chrome_path = '/usr/bin/google-chrome %s'
-   # webbrowser.get(chrome_path).open(url)
-
-client.loop_forever()
